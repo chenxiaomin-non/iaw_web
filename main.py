5826842e6c70e8fa94b2195b544908baaa3a9b54
Truong Tran <qw9mn@trf404.localdomain>
2022-09-23 17:09:32 +0700

    test: streamlit column

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import streamlit as st
+import hashlib
+
+def get_user_and_pass():
+    global username, password
+    pw = hashlib.md5(password)
+
+    return username, pw
+
 
 st.header("Login Page")
 st.text("\n\n")
 
-st.text_input("User: ")
+username = st.text_input("User: ")
 st.text("")
-st.text_input("Password: ")
+password = st.text_input("Password: ", type="password")
 st.text("")
 
-if st.button("<Login>"):
+col1, col2 = st.columns(2)
+login_bt = col1.button("<Login>")
+signin_bt = col2.button("<Sign in>")
+
+if login_bt:
     st.text("hello!")
-if st.button("<Sign in>"):
+    st.text(get_user_and_pass())
+if signin_bt:
     st.text("sign in")
